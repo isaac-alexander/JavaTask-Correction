323c9328b96f37b56738b64f80b12e5fd57c43ce
Isaac Alexander <Isaacalexander81@gmail.com>
2026-01-25 02:44:04 +0100

    src/Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+//
+
 
 public class Circle {
+    private int radius;
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    // method to get radius
+    public int getRadius() {
+        return radius;
+    }
+
+    // method to set radius
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    // Method to calculate area
+    int getArea() {
+        return (int) (Math.PI * radius * radius);
+    }
+
+    // Method to calculate circumference
+    int getCircumference() {
+        return (int) (2 * Math.PI * radius);
+    }
 }
